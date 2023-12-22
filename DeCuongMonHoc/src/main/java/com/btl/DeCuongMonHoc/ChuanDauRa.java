c32312ed81636ae8bb13dae3406cb2dfade3d855
duyan77 <fklt0961@gmail.com>
2023-12-22 23:19:59 +0700

    thêm getter, settet cho biến dem và bổ sung 2 method

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/ChuanDauRa.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/ChuanDauRa.java
@@ -14,6 +14,7 @@ public class ChuanDauRa {
         taoTenChuanDauRa();
     }
 
+    //    getter, setter
     public ChuanDauRa(MucTieu mucTieu) {
         this.mucTieu = mucTieu;
         taoTenChuanDauRa();
@@ -43,9 +44,28 @@ public class ChuanDauRa {
         this.mucTieu = mucTieu;
     }
 
+    public static int getDem() {
+        return dem;
+    }
+
+    public static void setDem(int dem) {
+        ChuanDauRa.dem = dem;
+    }
+
     private void taoTenChuanDauRa() {
         String tenMucTieu = this.mucTieu.getTenMucTieu();
         String mucTieuThu = tenMucTieu.substring(tenMucTieu.indexOf("O") + 1);
+        // CLO <=> course learning outcomes
         this.tenChuanDauRa = "CLO%s.%d".formatted(mucTieuThu, ++dem);
+    }
+
+    public void nhapChuanDauRa() {
+        System.out.printf("Nhập mô tả cho chuẩn đầu ra %s: ", this.tenChuanDauRa);
+        this.moTa = CauHinh.sc.nextLine();
+    }
+
+    @Override
+    public String toString() {
+        return "%s: %s\n".formatted(this.tenChuanDauRa, this.moTa);
     }
 }
