5033f005807ffb8e432a5d11b89e2381579aee17
duyan77 <fklt0961@gmail.com>
2023-12-22 16:14:48 +0700

    thêm constructor cho các lớp và chỉnh sửa 2 lớp ChuanDauRa và MucTieu

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/CotDiem.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/CotDiem.java
@@ -2,6 +2,10 @@ package com.btl.DeCuongMonHoc;
 
 public class CotDiem extends DiemThanhPhan {
 
+    public CotDiem(String ten, double tt) {
+        super(ten, tt);
+    }
+
     public void noiDungDanhGia() {
 
     }
