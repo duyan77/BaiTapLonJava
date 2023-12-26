336486ddc9b6289bc0252c9859c613d156a5aee1
duyan77 <fklt0961@gmail.com>
2023-12-26 11:41:35 +0700

    add method that return list of courses outline that lecture manages

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/PhanCong.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/PhanCong.java
@@ -1,13 +1,18 @@
 package com.btl.DeCuongMonHoc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PhanCong {
 
-    private List<GiangVien> dsGV;
+    private final List<GiangVien> phanCongGV = new ArrayList<>();
 
-    public List<DeCuongMonHoc> deCuongPhanCong(int maGV) {
-        return null;
+    // danh sach de cuong cua giang vien
+    public List<DeCuongMonHoc> danhSachDeCuong(String id) {
+        return this.phanCongGV.stream()
+                .filter(giangVien -> giangVien.getMaGV().equals(id))
+                .findFirst().orElseThrow().danhSachDeCuong();
     }
+
 
 }
