ad4ef3d751869cf49188c140080ba81df8881e4a
duyan77 <fklt0961@gmail.com>
2023-12-26 11:35:45 +0700

    remove redudant stament in class Main

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/Main.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/Main.java
@@ -2,9 +2,6 @@ package com.btl.DeCuongMonHoc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello 2nd");
-        System.out.println("tao nee");
 
     }
 }
