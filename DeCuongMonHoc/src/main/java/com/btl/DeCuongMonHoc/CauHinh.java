1a7bc278946cd4efabe19cc77a4d293661a6e6f4
duyan77 <fklt0961@gmail.com>
2023-12-27 02:08:44 +0700

    add field soLuongDeCuong in class CauHinh to limit the number of courses outline 1 lecture manages

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/CauHinh.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/CauHinh.java
@@ -4,5 +4,6 @@ import java.util.Scanner;
 
 public class CauHinh {
     public static final Scanner sc = new Scanner(System.in);
-    public static final int soLuong = 3;
+    public static final int soLuongDeCuong = 5;
+    public static final int soLuongMonDieuKien = 3;
 }
