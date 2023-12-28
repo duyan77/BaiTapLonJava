9ef3ab622fca0ea72099d758ae9f4286974d6535
duyan77 <fklt0961@gmail.com>
2023-12-28 23:21:12 +0700

    override toString

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/DanhGia.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/DanhGia.java
@@ -1,7 +1,9 @@
 package com.btl.DeCuongMonHoc;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 
 import static com.btl.DeCuongMonHoc.CauHinh.soCotDiemToiDa;
@@ -11,7 +13,7 @@ public class DanhGia {
 
     private int soCotDiem;
 
-    private List<CotDiem> cotDiem; // cot diem toi thieu 2 va toi da 4
+    private final List<CotDiem> cotDiem; // cot diem toi thieu 2 va toi da 4
 
     public DanhGia(int soCotDiem) {
         this.soCotDiem = soCotDiem;
@@ -33,7 +35,12 @@ public class DanhGia {
     }
 
     public void themCotDiem(CotDiem... cotDiems) {
-        this.cotDiem.addAll(List.of(cotDiems));
+        Arrays.stream(cotDiems).forEach(cotDiem1 -> {
+            if (this.cotDiem.size() >= soCotDiemToiDa)
+                throw new IllegalArgumentException("Số cột điểm tối thiểu 2 và tối đa 4!");
+            else this.cotDiem.add(cotDiem1);
+        });
+        this.soCotDiem = this.cotDiem.size();
     }
 
     public void themCotDiem() {
@@ -43,8 +50,22 @@ public class DanhGia {
         this.themCotDiem(tmp);
     }
 
+    // xoa hinh thuc danh gia
     public void xoaCotDiem(CotDiem cotDiem) {
         this.cotDiem.remove(cotDiem);
+        this.soCotDiem = this.cotDiem.size();
     }
-    
+
+    public void xoaCotDiem(int index) {
+        CotDiem cd = this.cotDiem.get(index);
+        if (cd == null) {
+            throw new IllegalArgumentException("Chi so de xoa diem khong dung!");
+        }
+        this.xoaCotDiem(cd);
+    }
+
+    @Override
+    public String toString() {
+        return this.cotDiem.stream().map(CotDiem::toString).collect(Collectors.joining());
+    }
 }
