f6b6564c618d1456154e1c7b0dda7a9a48acdfcc
duyan77 <fklt0961@gmail.com>
2024-01-04 14:34:25 +0700

    them Exception cho lop LimitedList

---
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/MaxSizeExceededException.java
@@ -0,0 +1,8 @@
+package com.btl.DeCuongMonHoc;
+
+public class MaxSizeExceededException extends RuntimeException {
+    public MaxSizeExceededException(String message) {
+        super(message);
+    }
+}
+
