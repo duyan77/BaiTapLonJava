c6eae01c3a1c5b1b3ba947f0ae8b7b735c74b5c9
yennhi2309 <nguyenthiyennhi23092004@gmail.com>
2024-01-05 22:28:23 +0700

    commit

--- DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/PhanCong.java
+++ DeCuongMonHoc/src/main/java/com/btl/DeCuongMonHoc/PhanCong.java
@@ -1,5 +1,8 @@
 package com.btl.DeCuongMonHoc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +24,43 @@ public class PhanCong {
     public GiangVien timGiangVien(String maGV) {
         return this.phanCongGV.stream().filter(giangVien -> giangVien.getMaGV().equals(maGV))
                 .findFirst().orElse(null);
+    }
+
+    public static List<GiangVien> docTuFile(String tenFile){
+        List<GiangVien> giangVienList = new ArrayList<>();
+        QuanLyDeCuong quanLyDeCuong = new QuanLyDeCuong();
+        FileReader fr = null;
+        BufferedReader br = null;
+        //doc file GiangVien
+        try {
+            fr = new FileReader(tenFile);
+            br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine())!=null){
+                String[] info = line.split("[|]");
+                String maGV = info[0].trim();
+                String tenGV = info[1].trim();
+                int soMon = Integer.parseInt(info[2].trim());
+                for(int i = 0; i<soMon; i++)
+                {
+                    String maMon = info[i+3].trim();
+                    quanLyDeCuong.timMonHoc(maMon);
+
+                }
+                GiangVien gv = new GiangVien(maGV);
+                giangVienList.add(gv);
+            }
+        } catch (IOException e) {
+            System.out.println("loi ghi file");
+        }finally {
+            try {
+                br.close();
+                fr.close();
+            } catch (IOException e) {
+                System.out.println("Loi dong file");
+            }
+        }
+        return giangVienList;
     }
 
     // danh sach de cuong cua giang vien
